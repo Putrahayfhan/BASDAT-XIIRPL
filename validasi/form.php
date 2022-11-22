790e806384b3c3c1bb32e6708f71494ddbc36198
Putrahayfhan <putrahayfhan2005@gmail.com>
2022-11-22 14:22:01 +0700

    lanjutan

--- validasi/form.php
+++ validasi/form.php
@@ -1,32 +1,57 @@
-<html>
-    <head>
-        <title>Validasi Form</title>
-    </head>
-    <body>
-        <form action="proses.php">
-            <div>
-                <div style="margin-top:10px;">
-                <label>Nama</label> <br>
-                <input type="text" name="nama">
-            </div>
-            <div>
-                <div style="margin-top:10px;">
-                <label>Email</label> <br>
-                <input type="text" name="email">
-            </div>
-            <div>
-                <div style="margin-top:10px;">
-                <label>Username</label> <br>
-                <input type="text" name="username">
-            </div>
-            <div>
-                <div style="margin-top:10px;">
-                <label>Usia</label> <br>
-                <input type="number" name="usia">
-            </div>
-            <div style="margin-top:10px;">
-                <button>Submit</button>
-            </div>
-        </form>
-    </body>
+<?php  
+    $time = (object) @$_GET['time'];
+    $errors = (object) @$_GET['errors'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validasi Form</title>
+</head>
+<body>
+    <form action="proses.php">
+        <div>
+            <label>Nama(Harus input nama)</label><br>
+            <input type="text" value="<?php echo @$time ->nama ?>" name="nama">
+            <?php
+            if (@$errors->nama):?>
+            <div style="color: red"><?php echo $errors->nama[0]?></div>
+            <?php
+            endif;?>
+        </div>
+        <div style="margin: top 10px;">
+            <label>Email (Harus input @)</label><br>
+            <input type="text" value="<?php echo @$time ->email ?>" name="email">
+            <?php
+            if (@$errors->email):?>
+            <div style="color: red"><?php echo $errors->email[0]?></div>
+            <?php
+            endif;?>
+        </div>
+        <div style="margin: top 10px;">
+            <label>Username(tidak boleh regex)</label><br>
+            <input type="text" value="<?php echo @$time ->username ?>" name="username">
+            <?php
+            if (@$errors->username):?>
+            <div style="color: red"><?php echo $errors->username[0]?></div>
+            <?php
+            endif;?>
+        </div>
+        <div style="margin: top 10px;">
+            <label>Usia(Harus Angka)</label><br>
+            <input type="text" value="<?php echo @$time ->usia ?>" name="usia">
+            <?php
+            if (@$errors->usia):?>
+            <div style="color: red"><?php echo $errors->usia[0]?></div>
+            <?php
+            endif;?>
+        </div><br>
+        <div style="margin: top 10px;">
+        <button>Submit</button>
+        </div>
+    </form>
+</body>
 </html>
